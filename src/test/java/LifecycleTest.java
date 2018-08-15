d5d1f99eb14ec84f29177eb8b5b6323826682f62
Ignat Simonenko <simonenkoignat@gmail.com>
2018-08-15 17:07:17 +0300

    Add test for JUnit5 test lifecycle

---
+++ src/test/java/LifecycleTest.java
@@ -0,0 +1,42 @@
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+@DisplayName("Lifecycle test")
+class LifecycleTest {
+
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("Before all test methods");
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        System.out.println("Before each test method");
+    }
+
+    @AfterEach
+    void afterEach() {
+        System.out.println("After each test method");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("After all test methods");
+    }
+
+    @Test
+    @DisplayName("First test")
+    void firstTest() {
+        System.out.println("First test method");
+    }
+
+    @Test
+    @DisplayName("Second test")
+    void secondTest() {
+        System.out.println("Second test method");
+    }
+}
